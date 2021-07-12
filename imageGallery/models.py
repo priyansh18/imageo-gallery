20156af188d3c4cf1bcf69b0db6dd184251cc6e5
priyansh18 <singhalpriyansh58@gmail.com>
2021-07-12 20:51:20 +0530

    Added Images and Tags modeland register in admin pannel

--- imageGallery/models.py
+++ imageGallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class GalleryImage(models.Model):
+    tags = models.ManyToManyField('Tags',blank=True)
+    image = models.ImageField(upload_to='media')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.image)
