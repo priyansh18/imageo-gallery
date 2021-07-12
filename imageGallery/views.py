a1265abf845123a82bc651aa968cc524c453a8fb
priyansh18 <singhalpriyansh58@gmail.com>
2021-07-12 21:23:37 +0530

    Pagination Added

--- imageGallery/views.py
+++ imageGallery/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import GalleryImage,Tags
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
 def gallery(request):
-    return render(request,'index.html',context={})
+    images = GalleryImage.objects.all()
+    p = Paginator(images, 2)
+    page_number = request.GET.get('page')
+    page_obj = p.get_page(page_number)  # returns the desired page object
+    context={
+        'images':images,
+        'page_obj': page_obj
+    }
+    return render(request,'index.html',context)
